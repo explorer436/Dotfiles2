3f25909a96a4df9e3f05fe5c2906428be5c16d76
Harsha Vardhan Edupuganti <explorer436@tutanota.com>
2022-02-23 11:50:00 -0500

    adding emacs config to the repo

---
+++ .emacs.d/init.el
@@ -0,0 +1,32 @@
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+(package-refresh-contents)
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package)
+)
+
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/my-config.org"))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("81c3de64d684e23455236abde277cda4b66509ef2c28f66e059aa925b8b12534" default))
+ '(org-agenda-files '("~/Downloads"))
+ '(package-selected-packages
+   '(js-import flex-autopair tide rjsx-mode ag ripgrep org-make-toc toc-org dap-java lsp-mode helm-lsp lsp-java lsp-ui yasnippet-snippets yasnippet company evil treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile auto-complete beacon emacs-color-themes emacs-color-theme which-key use-package try multiple-cursors magit helm haskell-mode dracula-theme avy)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
