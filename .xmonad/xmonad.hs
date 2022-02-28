43e4805574809a63108b8b1eb0e191eeb768ec70
Harsha Vardhan Edupuganti <explorer436@tutanota.com>
2022-02-27 22:20:32 -0500

    do not float xfce4-terminal and make it full screen instead

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -85,7 +85,7 @@ myManageHook = composeAll . concat $
     ]
     where
     -- doShiftAndGo = doF . liftM2 (.) W.greedyView W.shift
-    myCFloats = ["Arandr", "Arcolinux-calamares-tool.py", "Arcolinux-tweak-tool.py", "Arcolinux-welcome-app.py", "Galculator", "feh", "mpv", "Xfce4-terminal"]
+    myCFloats = ["Arandr", "Arcolinux-calamares-tool.py", "Arcolinux-tweak-tool.py", "Arcolinux-welcome-app.py", "Galculator", "feh", "mpv"]
     myTFloats = ["Downloads", "Save As..."]
     myRFloats = []
     myIgnores = ["desktop_window"]
