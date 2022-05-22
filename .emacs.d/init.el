3b9ed8911bc430653afadb6e18f8379206c6d4a1
explorer436 <explorer436@tutanota.com>
2022-05-22 14:56:56 -0400

    update emacs org agenda list by operating system

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,16 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/my-config.org"))
 
+(setq org-agenda-files
+      (if (eq system-type 'windows-nt)
+	  '("~/../../Downloads/GitRepositories/my-personal-things/Things to do/01ToDoList.org"
+	    "~/../../Downloads/GitRepositories/my-personal-things/birthdays/Birthdays.org")
+      '("~/Downloads/GitRepositories/my-personal-things/birthdays"
+        "~/Dokumente/Technik-u-Dokus/Emacs/Client1.org"
+	"~/Dokumente/Technik-u-Dokus/Emacs/Client2.org"
+	"~/Dokumente/RA-win/2012-222-name/file.org")
+      )
+    )
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -20,7 +30,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("81c3de64d684e23455236abde277cda4b66509ef2c28f66e059aa925b8b12534" default))
- '(org-agenda-files '("~/Downloads/GitRepositories/my-personal-things/birthdays"))
+;; '(org-agenda-files '("~/Downloads/GitRepositories/my-personal-things/birthdays"))
  '(package-selected-packages
    '(js-import flex-autopair tide rjsx-mode ag ripgrep org-make-toc toc-org dap-java lsp-mode helm-lsp lsp-java lsp-ui yasnippet-snippets yasnippet company evil treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile auto-complete beacon emacs-color-themes emacs-color-theme which-key use-package try multiple-cursors magit helm haskell-mode dracula-theme avy)))
 (custom-set-faces
