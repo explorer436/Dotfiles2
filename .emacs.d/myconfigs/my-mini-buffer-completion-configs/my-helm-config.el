377dd612544c61afdecf3512b9b959a6fec649c7
explorer436 <explorer436@tutanota.com>
2023-01-16 23:05:01 -0500

    use vertico instead of helm for mini buffer completion

---
+++ .emacs.d/myconfigs/my-mini-buffer-completion-configs/my-helm-config.el
@@ -0,0 +1,15 @@
+;;     (use-package helm
+;;       :init
+;;         (require 'helm-config)
+;;         (setq helm-split-window-in-side-p t
+;;               helm-move-to-line-cycle-in-source t)
+;;       :config 
+;;         (helm-mode 1) ;; Most of Emacs prompts become helm-enabled
+;;         (helm-autoresize-mode 1) ;; Helm resizes according to the number of candidates
+;;         (global-set-key (kbd "C-x b") 'helm-buffers-list) ;; List buffers ( Emacs way )
+;;         (global-set-key (kbd "C-x C-f") 'helm-find-files) ;; Finding files with Helm
+;;         (global-set-key (kbd "C-s") 'helm-occur)  ;; Replaces the default isearch keybinding
+;;         (global-set-key (kbd "C-h a") 'helm-apropos)  ;; Helmized apropos interface
+;;         (global-set-key (kbd "M-x") 'helm-M-x)  ;; Improved M-x menu
+;;         (global-set-key (kbd "M-y") 'helm-show-kill-ring)  ;; Show kill ring, pick something to paste
+;;       :ensure t)
