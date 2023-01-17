011150e6ece77952b5c2dd35e449eae5725b858d
explorer436 <explorer436@tutanota.com>
2023-01-16 23:30:11 -0500

    use consult package to find files in the current project

---
+++ .emacs.d/myconfigs/my-mini-buffer-completion-configs/my-consult-config.el
@@ -0,0 +1,13 @@
+;; https://github.com/minad/consult
+
+;; Enable consult
+(use-package consult
+  :ensure t
+)
+
+
+(defun fzf ()
+  "fuzzily finds files using consult-find"
+  (interactive)
+  (consult-find))
+
