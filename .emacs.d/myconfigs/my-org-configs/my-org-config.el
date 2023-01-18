cb436fdba757b30b2dd9940ff24e11504bf3f9bc
explorer436 <explorer436@tutanota.com>
2023-01-18 16:20:38 -0500

    splitting emacs config into smaller files

---
+++ .emacs.d/myconfigs/my-org-configs/my-org-config.el
@@ -0,0 +1,34 @@
+;; org-mode
+;; https://orgmode.org/
+(use-package org
+:ensure t
+:config
+  (setq org-log-done 'time) ;;keep track of when a certain TODO item was marked as done.
+)
+
+;; Agenda Views -
+;; First, you need to add your Org file to your Org agenda by using the command C-c [.
+;; Then, type Meta-x org-agenda to be presented with options for different views.
+      
+;; How can I show the Org-mode agenda on Emacs start-up?
+(add-hook 'after-init-hook 'org-agenda-list)
+
+;; Table of contents in org mode
+(use-package toc-org
+  :ensure t
+)
+
+(if (require 'toc-org nil t)
+(progn
+  (add-hook 'org-mode-hook 'toc-org-mode)
+
+  ;; enable in markdown, too
+  ;;(add-hook 'markdown-mode-hook 'toc-org-mode)
+  ;;(define-key markdown-mode-map (kbd "\C-c\C-o") 'toc-org-markdown-follow-thing-at-point)
+)
+(warn "toc-org not found"))
+
+;; 1. How to insert table of contents in an org file?
+;; 1. Put a heading in the document. After the line with the heading, add a ~TOC_5~ tag. Change 5 to something else based on the depth that you need for the table of contents.
+;; 1. To add a TOC tag, you can use the command ~org-set-tags-command~ (C-c C-q)
+;; 1. Enter TOC
---
+++ .emacs.d/myconfigs/my-org-configs/my-org-roam-config.el
@@ -0,0 +1,64 @@
+;; org-roam
+;; https://github.com/org-roam/org-roam
+;; https://www.orgroam.com/manual.html#Keystroke-Index
+;; Prerequisites: Install Graphviz for the Linux distribution
+
+;; Troubleshooting for the error "Symbol’s function definition is void"
+;; Install org-roam using this first.
+;; And then, use the next script for customizing it.
+;; It seems to be having trouble if we try to do all of it at once.
+
+(use-package org-roam
+ :ensure t)
+
+;; After making sure that org-roam is installed successfully, then run the following script for customization.
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/Downloads/GitRepositories/my-personal-wiki"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+   ("C-c n f" . org-roam-node-find)
+   ("C-c n g" . org-roam-graph)
+   ("C-c n i" . org-roam-node-insert)
+   ("C-c n c" . org-roam-capture)
+   ;; Dailies
+   ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+)
+
+;; 1. "org-roam-node-insert" to insert a node in another node. creates a node if it doesn not already exist.
+;; 1. "org-roam-node-find" to create/insert a new node if it does not already exist; and visits the node.
+;; 1. "org-roam-capture" creates a node if it does not already exist; restores the current window configuration upon completion.
+;; 1. "org-roam-buffer-toggle" to open backlinks buffer for a node
+;; 1. "org-id-get-create" to convert headings in an org file into nodes (instead of creating separate files for small sections)
+;; 1. "org-roam-graph" to launch a graph view of the org-roam notes
+
+;; How to rename a note with everything updated at the same time? The references don't seem to get updated. Find a good solution for this.
+
+;; Rename it within emacs e.g. using dired.
+;; Org-roam notices when renaming happens within emacs and will update your links accordingly.
+;; If you do it through a file editor, it has no idea!
+
+;; Rename the #+TITLE in your file and you should see the change in org-roam-find-file. You shouldn’t need to run org-roam-db-build-cache.
+
+;; If you want the description of links in all of your org-roam files to reflect the TITLE change as well as the filename change – [[file:AAA.org][AAA]] to [[file:ABC.org][ABC]] – looking in the code I think if you rename TITLE first, and then rename the file through dired, your link descriptions should be updated also.
+
+
+
+
+;; org-roam-ui
+;; https://github.com/org-roam/org-roam-ui
+;; Prerequisites: org-roam
+
+(use-package org-roam-ui
+    :ensure t
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
+;; Just use "Meta-X org-roam-ui-open" and it will launch a tab in the browser with this url: http://localhost:35901/
