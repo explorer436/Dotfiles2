527c400c2ab377ee0c9a3bd174d6fbb3d1f0befd
explorer436 <explorer436@tutanota.com>
2023-01-19 17:13:01 -0500

    enable consult package

--- .emacs.d/myconfigs/my-mini-buffer-completion-configs/my-consult-config.el
+++ .emacs.d/myconfigs/my-mini-buffer-completion-configs/my-consult-config.el
@@ -1,13 +1,11 @@
 ;; https://github.com/minad/consult
 
 ;; Enable consult
-;; (use-package consult
-;;   :ensure t
-;; )
-;; 
-;; 
+(use-package consult
+  :ensure t
+)
+
 ;; (defun fzf ()
 ;;   "fuzzily finds files using consult-find"
 ;;   (interactive)
 ;;   (consult-find))
-
