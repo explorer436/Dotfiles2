c4ac121c828970aaa2015496d198e1f18d86df4f
explorer436 <explorer436@tutanota.com>
2023-02-06 15:38:11 -0500

    add notes about refiling

--- .emacs.d/myconfigs/my-org-configs/02my-org-agenda-config.el
+++ .emacs.d/myconfigs/my-org-configs/02my-org-agenda-config.el
@@ -3,6 +3,27 @@
 	  '("~/../../Downloads/GitRepositories/my-personal-things/Things to do/01ToDoList.org"
 	    "~/../../Downloads/GitRepositories/my-personal-things/birthdays/Birthdays.org")
       '("~/Downloads/GitRepositories/my-personal-things/Things to do/01ToDoList.org"
+        "~/Downloads/GitRepositories/my-personal-things/Things to do/CompletedMiscellaneousTasks.org"
         "~/Downloads/GitRepositories/my-personal-things/birthdays/Birthdays.org")
       )
     )
+
+; Targets include this file and any file contributing to the agenda - up to 9 levels deep
+(setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                 (org-agenda-files :maxlevel . 9))))
+
+;; Refile Targets
+
+;; https://orgmode.org/manual/Refile-and-Copy.html
+;; https://blog.aaronbieber.com/2017/03/19/organizing-notes-with-refile.html
+
+;; First, we must set the value of org-refile-targets, which tells Org how to find the possible targets. Be sure to read the documentation for this variable, because there are a few ways to define targets, but for this scenario, I want to be able to move a node to any of my agenda files, and I also want to consider nodes up to three levels deep.
+
+;; That’s exactly what this setting will do.
+
+;; (setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
+
+;; With this set, you can trigger Refile with C-c C-w in any Org file and get a completing read of all headings up to three levels deep in all files in org-agenda-files.
+
+;; If you use Helm, which you really should, you will see a very pretty hierarchical display of possible targets. You can type a filename to narrow down to headings within that file, and so on.
+
