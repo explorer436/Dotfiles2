3ebffa9a744350082acc0258b6badf108bd966d1
explorer436 <explorer436@tutanota.com>
2023-02-08 19:44:09 -0500

    update org agenda list

--- .emacs.d/myconfigs/my-org-configs/02my-org-agenda-config.el
+++ .emacs.d/myconfigs/my-org-configs/02my-org-agenda-config.el
@@ -1,10 +1,12 @@
 (setq org-agenda-files
       (if (eq system-type 'windows-nt)
 	  '("~/../../Downloads/GitRepositories/my-personal-things/Things to do/01ToDoList.org"
-	    "~/../../Downloads/GitRepositories/my-personal-things/Things to do/01ToDoList.org"
+	    "~/../../Downloads/GitRepositories/my-personal-things/Things to do/CompletedMiscellaneousTasks.org"
+	    "~/../../Downloads/GitRepositories/my-personal-things/Things to do/BookNotes.org"
 	    "~/../../Downloads/GitRepositories/my-personal-things/birthdays/Birthdays.org")
       '("~/Downloads/GitRepositories/my-personal-things/Things to do/01ToDoList.org"
         "~/Downloads/GitRepositories/my-personal-things/Things to do/CompletedMiscellaneousTasks.org"
+        "~/Downloads/GitRepositories/my-personal-things/Things to do/BookNotes.org"
         "~/Downloads/GitRepositories/my-personal-things/birthdays/Birthdays.org")
       )
     )
