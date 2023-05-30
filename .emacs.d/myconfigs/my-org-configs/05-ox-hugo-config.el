de47359687dc49113c6d7e45d207441b99b7a2c8
explorer436 <explorer436@tutanota.com>
2023-05-30 11:50:35 -0400

    add ox-hugo config

---
+++ .emacs.d/myconfigs/my-org-configs/05-ox-hugo-config.el
@@ -0,0 +1,8 @@
+(use-package ox-hugo
+  :ensure t   ;Auto-install the package from Melpa
+  :pin melpa  ;`package-archives' should already have ("melpa" . "https://melpa.org/packages/")
+  :after ox)
+
+
+;; https://ox-hugo.scripter.co/doc/org-toc/
+(setq org-hugo-export-with-toc t)
