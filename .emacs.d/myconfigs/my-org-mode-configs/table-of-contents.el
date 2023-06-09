3427b192be8fec3403ed867f9b431863f69b79e4
explorer436 <explorer436@tutanota.com>
2023-06-09 05:23:28 -0400

    update org mode configs

--- .emacs.d/myconfigs/my-org-mode-configs/table-of-contents.el
+++ .emacs.d/myconfigs/my-org-mode-configs/table-of-contents.el
@@ -1,14 +1,3 @@
-;; org-mode
-;; https://orgmode.org/
-(use-package org
-:ensure t
-:config
-  (setq org-log-done 'time) ;;keep track of when a certain TODO item was marked as done.
-)
-
-;; How can I show the Org-mode agenda on Emacs start-up?
-(add-hook 'after-init-hook 'org-agenda-list)
-
 ;; Table of contents in org mode
 (use-package toc-org
   :ensure t
