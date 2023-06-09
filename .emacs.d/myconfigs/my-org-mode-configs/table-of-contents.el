a30c23a51e29e7861c694c65c497dc1e2f3705b3
explorer436 <explorer436@tutanota.com>
2023-06-09 08:27:00 -0400

    update toc notes

--- .emacs.d/myconfigs/my-org-mode-configs/table-of-contents.el
+++ .emacs.d/myconfigs/my-org-mode-configs/table-of-contents.el
@@ -17,3 +17,13 @@
 ;; 1. Put a heading in the document. After the line with the heading, add a ~TOC_5~ tag. Change 5 to something else based on the depth that you need for the table of contents.
 ;; 1. To add a TOC tag, you can use the command ~org-set-tags-command~ (C-c C-q)
 ;; 1. Enter TOC
+
+;; Advantages of using toc-org
+;; 1. Nice table of contents view in Github
+
+;; Disadvantages
+;; 1. Issues while exporting org documents as html or markdown format.
+;;    Emacs shows this error:
+;;    Unable to resolve link: (item from table of contents)
+
+;; If we are exporting org documents to html using emacs built-in export option, toc is not necessary. Because, we can auto-generate it using org-publish-project-alist's ":with-toc" setting.
