5b63cb977bd59c0089e580682dcc464f692eb4b0
explorer436 <explorer436@tutanota.com>
2023-07-15 19:12:11 -0400

    update roam config

--- .emacs.d/myconfigs/my-org-mode-configs/my-org-roam-config.el
+++ .emacs.d/myconfigs/my-org-mode-configs/my-org-roam-config.el
@@ -19,7 +19,7 @@
 (use-package org-roam
   :ensure t
   :custom
-  (org-roam-directory (file-truename "~/Downloads/GitRepositories/nextjs-markdown-blog/posts"))
+  (org-roam-directory (file-truename "~/Downloads/GitRepositories/hugo-blog/org"))
   :bind (("C-c n l" . org-roam-buffer-toggle)
    ("C-c n f" . org-roam-node-find)
    ("C-c n g" . org-roam-graph)
