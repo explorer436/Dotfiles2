746ff99b6d1e5f51540d78a081a72c06ba104a6e
explorer436 <explorer436@tutanota.com>
2023-09-15 19:56:50 -0400

    update emacs config

--- .emacs.d/myconfigs/my-mini-buffer-completion-configs/marginalia.el
+++ .emacs.d/myconfigs/my-mini-buffer-completion-configs/marginalia.el
@@ -1,5 +1,8 @@
 ;; Enable rich annotations using the Marginalia package
 (use-package marginalia
+
+  :ensure t
+
   ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
   ;; available in the *Completions* buffer, add it to the
   ;; `completion-list-mode-map'.
