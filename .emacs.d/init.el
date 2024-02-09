ef88824cc2270419836b153a3eb868853cf6f3bf
explorer436 <explorer436@tutanota.com>
2024-02-09 16:40:31 -0500

    using org-timeblock

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,6 +16,12 @@
 (use-package delight :ensure t)
 (use-package use-package-ensure-system-package :ensure t)
 
+(use-package quelpa-use-package
+  :ensure t
+  :init
+  (setq quelpa-update-melpa-p nil)
+  (setq quelpa-self-upgrade-p nil))
+
 ;; Recursively load config definitions from a folder/folders recursively
 ;;https://www.emacswiki.org/emacs/DotEmacsModular
 (load "~/.emacs.d/load-directory")
@@ -36,7 +42,7 @@
  '(elfeed-feeds '("https://blog.osm-ai.net/feed.xml"))
  '(org-export-backends '(ascii html icalendar latex md odt))
  '(package-selected-packages
-   '(org-modern org-caldav elfeed-org elfeed calfw-org calfw org-tree-slide string-inflection embark-consult embark whitespace-cleanup-mode soft-morning-theme firecode-theme marginalia orderless vertico htmlize ox-hugo consult rg js-import flex-autopair tide rjsx-mode ag ripgrep org-make-toc toc-org dap-java lsp-mode helm-lsp lsp-java lsp-ui yasnippet-snippets yasnippet company evil treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile auto-complete beacon emacs-color-themes emacs-color-theme which-key use-package try multiple-cursors magit helm haskell-mode dracula-theme avy)))
+   '(quelpa-use-package org-timeblock org-modern org-caldav elfeed-org elfeed calfw-org calfw org-tree-slide string-inflection embark-consult embark whitespace-cleanup-mode soft-morning-theme firecode-theme marginalia orderless vertico htmlize ox-hugo consult rg js-import flex-autopair tide rjsx-mode ag ripgrep org-make-toc toc-org dap-java lsp-mode helm-lsp lsp-java lsp-ui yasnippet-snippets yasnippet company evil treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile auto-complete beacon emacs-color-themes emacs-color-theme which-key use-package try multiple-cursors magit helm haskell-mode dracula-theme avy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ .emacs.d/myconfigs/my-org-mode-configs/my-org-timeblock-config.el
@@ -0,0 +1,11 @@
+;; https://github.com/ichernyshovvv/org-timeblock
+
+;; Commands
+;; Run M-x org-timeblock to open 3-day view that starts from today’s date.
+
+;; org-timeblock-mode. Displays timeblock view of SCHEDULED/DEADLINE org tasks or events (entries that have active timestamps in the body or heading) for specific days.
+;; org-timeblock-list-mode. Displays a list of tasks (including those that are not time-specific).
+
+(use-package org-timeblock
+  :ensure t
+  :quelpa (org-timeblock :fetcher github :repo "ichernyshovvv/org-timeblock"))
