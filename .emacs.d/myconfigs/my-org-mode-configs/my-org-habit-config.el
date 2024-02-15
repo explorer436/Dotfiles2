0594578c237abd47b54514da9de3d188dfa32352
explorer436 <explorer436@tutanota.com>
2024-02-15 17:15:28 -0500

    disable auto sync for org-caldav

--- .emacs.d/myconfigs/my-org-mode-configs/my-org-habit-config.el
+++ .emacs.d/myconfigs/my-org-mode-configs/my-org-habit-config.el
@@ -1,9 +1,11 @@
-  (use-package org-habit
-    :ensure nil
-    :after org
-    :commands (org-habit-toggle-habits org-habit-toggle-display-in-agenda)
-    :config (progn
-	      (add-to-list 'org-modules 'org-habit)
-	      (setq org-habit-graph-column 80)
-	    )
-    (message "org-habit ready ✔"))
+;; In the graph, "Today" is represented by !
+
+(use-package org-habit
+  :ensure nil
+  :after org
+  :commands (org-habit-toggle-habits org-habit-toggle-display-in-agenda)
+  :config (progn
+      (add-to-list 'org-modules 'org-habit)
+      (setq org-habit-graph-column 80)
+    )
+  (message "org-habit ready ✔"))
