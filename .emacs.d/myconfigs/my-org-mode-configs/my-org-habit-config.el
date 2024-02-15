56587ba565d16983d229a6189dd464d5f452d2a3
explorer436 <explorer436@tutanota.com>
2024-02-15 12:00:17 -0500

    add org-habit config

---
+++ .emacs.d/myconfigs/my-org-mode-configs/my-org-habit-config.el
@@ -0,0 +1,9 @@
+  (use-package org-habit
+    :ensure nil
+    :after org
+    :commands (org-habit-toggle-habits org-habit-toggle-display-in-agenda)
+    :config (progn
+	      (add-to-list 'org-modules 'org-habit)
+	      (setq org-habit-graph-column 80)
+	    )
+    (message "org-habit ready ✔"))
