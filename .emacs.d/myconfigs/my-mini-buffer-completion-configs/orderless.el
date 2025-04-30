408e0bb0a3238ddf84e314f2067c61a54183411a
explorer436 <explorer436@tutanota.com>
2025-04-30 17:47:41 -0400

    commenting out mini buffer customizations

--- .emacs.d/myconfigs/my-mini-buffer-completion-configs/orderless.el
+++ .emacs.d/myconfigs/my-mini-buffer-completion-configs/orderless.el
@@ -1,5 +1,5 @@
-(use-package orderless
-  :ensure t
-  :custom
-  (completion-styles '(orderless basic))
-  (completion-category-overrides '((file (styles basic partial-completion)))))
+;; (use-package orderless
+;;   :ensure t
+;;   :custom
+;;   (completion-styles '(orderless basic))
+;;   (completion-category-overrides '((file (styles basic partial-completion)))))
