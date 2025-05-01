436f2a831386d019fa1e66ca103f1e4e0e0b2e7f
explorer436 <explorer436@tutanota.com>
2025-04-30 22:23:39 -0400

    update emacs config

--- .emacs.d/myconfigs/my-mini-buffer-completion-configs/orderless.el
+++ .emacs.d/myconfigs/my-mini-buffer-completion-configs/orderless.el
@@ -1,5 +1,10 @@
-;; (use-package orderless
-;;   :ensure t
-;;   :custom
-;;   (completion-styles '(orderless basic))
-;;   (completion-category-overrides '((file (styles basic partial-completion)))))
+;; https://github.com/oantolin/orderless
+
+;; When you type a few characters in the mini buffer, it shows all the possible function names.
+;; Very useful to recall any of the available functions from the mini buffer.
+
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
