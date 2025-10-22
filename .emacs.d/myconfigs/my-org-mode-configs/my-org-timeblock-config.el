c8d23f8f322f5109947c2c98c991750b8c388f42
explorer436 <explorer436@tutanota.com>
2025-10-22 14:35:58 -0400

    update emacs config

--- .emacs.d/myconfigs/my-org-mode-configs/my-org-timeblock-config.el
+++ .emacs.d/myconfigs/my-org-mode-configs/my-org-timeblock-config.el
@@ -6,18 +6,24 @@
 ;; org-timeblock-mode. Displays timeblock view of SCHEDULED/DEADLINE org tasks or events (entries that have active timestamps in the body or heading) for specific days.
 ;; org-timeblock-list-mode. Displays a list of tasks (including those that are not time-specific).
 
-(use-package org-timeblock
-  :ensure t
-  :quelpa (org-timeblock :fetcher github :repo "ichernyshovvv/org-timeblock")
-)
+;; How can I show the Org-mode agenda on Emacs start-up? (add-hook 'after-init-hook 'my-timeblock-views)
 
-(defun my-timeblock-views ()
-  "Launch org-timeblock and org-timeblock-toggle-timeblock-list simulataneously"
-  (interactive)
-  (org-timeblock)
-  (org-timeblock-toggle-timeblock-list)
-  (kill-buffer "*org-timeblock*")
-)
+;; Errors with this package
+;; I am seeing this error
+;; load-with-code-conversion: Cannot open load file: No such file or directory, compat-macs
+;; Not sure how to fix it. So, I am disabling this package.
 
-;; How can I show the Org-mode agenda on Emacs start-up?
-;; (add-hook 'after-init-hook 'my-timeblock-views)
+
+
+;; (use-package org-timeblock
+;;   :ensure t
+;;   :quelpa (org-timeblock :fetcher github :repo "ichernyshovvv/org-timeblock")
+;; )
+
+;; (defun my-timeblock-views ()
+;;   "Launch org-timeblock and org-timeblock-toggle-timeblock-list simulataneously"
+;;   (interactive)
+;;   (org-timeblock)
+;;   (org-timeblock-toggle-timeblock-list)
+;;   (kill-buffer "*org-timeblock*")
+;; )
