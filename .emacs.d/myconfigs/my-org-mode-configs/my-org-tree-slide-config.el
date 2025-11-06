b18ad7e5786ac25b0717aeaf6a434dce56102ef4
explorer436 <explorer436@tutanota.com>
2025-11-06 14:57:50 -0500

    update notes

--- .emacs.d/myconfigs/my-org-mode-configs/my-org-tree-slide-config.el
+++ .emacs.d/myconfigs/my-org-mode-configs/my-org-tree-slide-config.el
@@ -1,7 +1,15 @@
+;; Turn any org document into a presentation by doing: ~M-x org-tree-slide-mode~
 ;; https://github.com/takaxp/org-tree-slide
+
 (use-package org-tree-slide
   :ensure t
   :custom
   (org-image-actual-width nil))
 ;; do not let large images take up the entire screen or more size than the screen.
-;; Turn any org document into a presentation by doing: ~M-x org-tree-slide-mode~
+
+;; How to move forward and backward in the presentation?
+;; https://github.com/takaxp/org-tree-slide?tab=readme-ov-file#control-functions
+;; Control functions
+;; 1. org-tree-slide-move-next-tree (C->)
+;; 1. org-tree-slide-move-previous-tree (C-<)
+;; 1. org-tree-slide-content (C-x s c)
